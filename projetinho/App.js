92d2b1bfebc28e6c84b08c9caab0b2e4a69e4106
Lucas Ribeiro Nicoleti <lucasribeironicoleti@gmail.com>
2022-07-10 20:05:53 -0300

    Estudando React part 3

--- projetinho/App.js
+++ projetinho/App.js
@@ -1,20 +1,44 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          /* 1. Navigate to the Details route with params */
+          navigation.navigate('Details', {
+            itemId: 86,
+            otherParam: 'anything you want here',
+          });
+        }}
+      />
     </View>
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({ route, navigation }) {
+  /* 2. Get the param */
+  const { itemId, otherParam } = route.params;
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() =>
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+          })
+        }
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
@@ -32,7 +56,9 @@ function App(){
   );
 }
 
-{/* <Stack.Screen name="Home" component={HomeScreen} option={{ title: 'Overview' }} /> */}
+{
+   <Stack.Screen name="Home" component={HomeScreen} option={{ title: 'Overview' }} /> 
+}
 
 <Stack.Screen name="Home">
   {props => <HomeScreen {...props} extraData={someData} />}
@@ -51,3 +77,40 @@ export default App;
 
 
 
+// function HomeScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Home Screen</Text>
+//       <Button title="Go to Details" onPress={() => navigation.navigate('Details')} />
+//     </View>
+//   );
+// }
+
+// function DetailsScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Details Screen</Text>
+//       <Button title="Go to datails... again" onPress={() => navigation.push('Details')} />
+//       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+//       <Button title="Go back" onPress={() => navigation.goBack()} />
+//       <Button title="Go back to first screen in stack" onPress={() => navigation.popToTop()} />
+//     </View>
+//   );
+// }
+
+// const Stack = createNativeStackNavigator();
+
+// function App(){
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName='Home'>
+//         <Stack.Screen name="Home" component={HomeScreen} />
+//         <Stack.Screen name="Details" component={DetailsScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+// {
+//    <Stack.Screen name="Home" component={HomeScreen} option={{ title: 'Overview' }} /> 
+// }
